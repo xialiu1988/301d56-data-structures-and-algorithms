581f679d79cf6135c766cfaaad389fa314b85d51
xialiu1988 <liuxia198896@gmail.com>
2019-07-02 13:11:02 -0700

    updated readme

---
+++ 401code_challenges/codeChallenge/src/main/java/mergeSort/App.java
@@ -0,0 +1,22 @@
+package mergeSort;
+
+public class App {
+
+
+    public static void main(String[] args){
+
+        int[] arr= {45,12,9,20};
+       int[] res= mergeSort(arr);
+       for(int a: res){
+           System.out.println(a);
+       }
+    }
+
+
+    public static int[] mergeSort(int[] arr){
+
+
+    }
+
+
+}
